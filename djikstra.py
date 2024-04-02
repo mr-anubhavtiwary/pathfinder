310890c8ff50c5e4fa51dc67833e5e1c827a7de3
Anubhav <mr.anubhavtiwary@gmail.com>
2024-04-02 23:17:58 +0530

    added dfs algorithm

--- djikstra.py
+++ djikstra.py
@@ -40,7 +40,7 @@ class Box:
         #     self.neighbours.append(grid[self.x + 1][self.y + 1])    # down-right
 
 # handling mouse click events
-def handle_mouse_click(event, grid, start_box_set, target_box_set, start_box, target_box, queue, box_width, box_height, start_box_col, start_box_row, target_box_col, target_box_row):
+def handle_mouse_click(event, grid, start_box_set, target_box_set, start_box, target_box, list, box_width, box_height, start_box_col, start_box_row, target_box_col, target_box_row):
     if event.button == 1 and not start_box_set:     # 1st left click
         start_box_col = event.pos[0] // box_width
         start_box_row = event.pos[1] // box_height
@@ -48,7 +48,7 @@ def handle_mouse_click(event, grid, start_box_set, target_box_set, start_box, ta
         start_box.start = True
         start_box.visited = True
         start_box_set = True
-        queue.append(start_box)
+        list.append(start_box)
         
     elif event.button == 1 and not target_box_set:      # 2nd left click
         target_box_col = event.pos[0] // box_width
@@ -65,6 +65,60 @@ def handle_mouse_click(event, grid, start_box_set, target_box_set, start_box, ta
                 grid[col][row].wall = False
         
     return start_box_set, target_box_set, start_box, target_box, start_box_col, start_box_row, target_box_col, target_box_row
+
+# djikstra algorithm
+def djikstra(list, start_box, target_box, searching, path):
+    if len(list) > 0 and searching:
+        current_box = list.pop(0)
+        current_box.visited = True
+        
+        if current_box == target_box:
+            searching = False
+            
+            while current_box.prior != start_box:
+                path.append(current_box.prior)
+                current_box = current_box.prior
+            return searching
+        else:
+            for neighbour in current_box.neighbours:
+                if not neighbour.queued and not neighbour.wall:
+                    neighbour.queued = True
+                    neighbour.prior = current_box
+                    list.append(neighbour)
+    else:
+        if searching:
+            Tk().wm_withdraw()
+            messagebox.showinfo("Alert", "No path Found!")
+            searching = False
+            
+    return searching
+
+# depth first search algorithm
+def dfs(list, start_box, target_box, searching, path):
+    if len(list) and searching:
+        current_box = list.pop()
+        current_box.visited = True
+        
+        if current_box == target_box:
+            searching = False
+            while current_box.prior != start_box:
+                path.append(current_box.prior)
+                current_box = current_box.prior
+            path.reverse()
+            return searching
+        
+        for neighbour in current_box.neighbours:
+            if not neighbour.queued and not neighbour.wall:
+                neighbour.queued = True
+                neighbour.prior = current_box
+                list.append(neighbour)
+    else:        
+        if searching:
+            Tk().wm_withdraw()
+            messagebox.showinfo("Alert", "No path Found!")
+            searching = False
+    
+    return searching
 
 def main():
     # initializing window size
@@ -87,11 +141,11 @@ def main():
         for row in range(rows):
             grid[col][row].set_neighbours(grid, columns, rows)
             
-    queue = []                      # for neighbours
+    list = []                      # for neighbours
     path = []                       # for final path
     start_box_set = False
     target_box_set = False
-    begin_search = False
+    begin_search = 0
     searching = True
     start_box = None
     start_box_col = None
@@ -108,7 +162,7 @@ def main():
                 sys.exit()
                     
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                start_box_set, target_box_set, start_box, target_box, start_box_col, start_box_row, target_box_col, target_box_row = handle_mouse_click(event, grid, start_box_set, target_box_set, start_box, target_box, queue, box_width, box_height, start_box_col, start_box_row, target_box_col, target_box_row)
+                start_box_set, target_box_set, start_box, target_box, start_box_col, start_box_row, target_box_col, target_box_row = handle_mouse_click(event, grid, start_box_set, target_box_set, start_box, target_box, list, box_width, box_height, start_box_col, start_box_row, target_box_col, target_box_row)
             
             elif event.type == pygame.MOUSEMOTION:
                 x = pygame.mouse.get_pos()[0]
@@ -129,33 +183,22 @@ def main():
                 if event.key == pygame.K_r:
                     main()
                 
-                # algorithm initialization key = space
-                elif event.key == pygame.K_SPACE and target_box_set:
-                    begin_search = True
+                # algorithms initialization keys
+                elif event.key == pygame.K_1 and target_box_set:    # djikstra algorithm
+                    begin_search = 1
+                elif event.key == pygame.K_2 and target_box_set:    # depth first search algorithm
+                    begin_search = 2
+                elif event.key == pygame.K_3 and target_box_set:    # breadth first search algorithm
+                    begin_search = 3
+                elif event.key == pygame.K_4 and target_box_set:    # (A*) algorithm
+                    begin_search = 4
         
-        # Djikstra algorithm
-        if begin_search:
-            if len(queue) > 0 and searching:
-                current_box = queue.pop(0)
-                current_box.visited = True
-                
-                if current_box == target_box:
-                    searching = False
-                    
-                    while current_box.prior != start_box:
-                        path.append(current_box.prior)
-                        current_box = current_box.prior
-                else:
-                    for neighbour in current_box.neighbours:
-                        if not neighbour.queued and not neighbour.wall:
-                            neighbour.queued = True
-                            neighbour.prior = current_box
-                            queue.append(neighbour)
-            else:
-                if searching:
-                    Tk().wm_withdraw()
-                    messagebox.showinfo("Alert", "No path Found!")
-                    searching = False
+        # pathfinding algorithms
+        if begin_search == 1:
+            searching = djikstra(list, start_box, target_box, searching, path)
+            
+        elif begin_search == 2:
+            searching = dfs(list, start_box, target_box, searching, path)
         
         # creating blank window
         window.fill((120, 120, 120))
@@ -180,6 +223,7 @@ def main():
                     box.draw(window, (244, 36, 102), box_width, box_height)
         
         pygame.display.flip()
+        # pygame.time.delay(100)
 
 if __name__ == "__main__":
     main()
