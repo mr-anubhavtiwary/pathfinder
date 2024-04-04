6f79c47b7eb348b1cff5c6744664bda6032d41b5
Anubhav <mr.anubhavtiwary@gmail.com>
2024-04-04 11:42:15 +0530

    tweaked performance

--- djikstra.py
+++ djikstra.py
@@ -1,6 +1,7 @@
 # modules
 import pygame
 import sys
+import math
 from tkinter import messagebox, Tk
 
 pygame.init()           # initialize pygame 
@@ -29,7 +30,7 @@ class Box:
     
     # simple functions
     def get_pos(self):                      # returns the position of box
-        return self.col, self.row
+        return self.x, self.y
     
     def is_visited(self):                   # returns if box is visited or not
         return self.visited
@@ -49,32 +50,34 @@ class Box:
     def get_prior(self):                    # returns the previous box of the current box
         return self.prior
     
-    def set_visited(self):                  # marks the box as visited
-        self.visited = True
+    def set_visited(self, val):                  # marks the box as visited
+        self.visited = val
     
-    def set_queued(self):                   # marks the box as queued
-        self.queued = True
+    def set_queued(self, val):                   # marks the box as queued
+        self.queued = val
     
-    def set_wall(self):                     # marks the box as a wall
-        self.wall = True
+    def set_wall(self, val):                     # marks the box as a wall
+        self.wall = val
         
-    def set_start(self):                    # marks the box as a start box
-        self.start = True
+    def set_start(self, val):                    # marks the box as a start box
+        self.start = val
         
-    def set_target(self):                   # marks the box as a target box
-        self.target = True
+    def set_target(self, val):                   # marks the box as a target box
+        self.target = val
         
     def set_prior(self, prior):             # sets the prior of the box as given box
         self.prior = prior
     
-    def remove_wall(self):                  # removes the box status as a wall
-        self.wall = False
+    
+    # def __lt__(self, other):
+    #     return False
     
     # drawing individual boxes in window
     def draw(self, win, color, box_width, box_height):
         # subtracting 2 pixels to show border
         pygame.draw.rect(win, color, (self.x * box_width, self.y * box_height, box_width - 2, box_height - 2))
 
+    # 4-way neighbour / 8-way neighbour
     def set_neighbours(self, grid, columns, rows):
         if self.x > 0:
             self.neighbours.append(grid[self.x - 1][self.y])   # left box
@@ -93,38 +96,69 @@ class Box:
         # if self.x < columns - 1 and self.y < rows - 1:
         #     self.neighbours.append(grid[self.x + 1][self.y + 1])    # down-right
 
+# heuristic function
+def h(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2)      # manhattan distance
+
 # handling mouse click events
-def handle_mouse_click(event, grid, start_box_set, target_box_set, start_box, target_box, list, box_width, box_height, start_box_col, start_box_row, target_box_col, target_box_row):
+def handle_mouse_click(event, grid, start_box_set, target_box_set, start_box, target_box, list, box_width, box_height):
     if event.button == 1 and not start_box_set:     # 1st left click
-        start_box_col = event.pos[0] // box_width
-        start_box_row = event.pos[1] // box_height
-        start_box = grid[start_box_col][start_box_row]
-        start_box.set_start()
-        start_box.set_visited()
-        start_box_set = True
-        list.append(start_box)
+        col = event.pos[0] // box_width
+        row = event.pos[1] // box_height
+        # if start and target are not same and box is not a wall
+        if not target_box_set or (target_box_set and (col, row) != target_box.get_pos() and not grid[col][row].is_wall()):
+            start_box_col = col
+            start_box_row = row
+            start_box = grid[start_box_col][start_box_row]
+            start_box.set_start(True)
+            start_box.set_visited(True)
+            start_box_set = True
+            list.append(start_box)
         
     elif event.button == 1 and not target_box_set:      # 2nd left click
-        target_box_col = event.pos[0] // box_width
-        target_box_row = event.pos[1] // box_height
-        target_box = grid[target_box_col][target_box_row]
-        target_box.set_target()
-        target_box_set = True
+        col = event.pos[0] // box_width
+        row = event.pos[1] // box_height
+         # if start and target are not same and box is not a wall
+        if not start_box_set or (start_box_set and (col, row) != start_box.get_pos() and not grid[col][row].is_wall()):
+            target_box_col = col
+            target_box_row = row
+            target_box = grid[target_box_col][target_box_row]
+            target_box.set_target(True)
+            target_box_set = True
         
     elif event.button == 3:  # right-click to remove obstacles
         col = event.pos[0] // box_width
         row = event.pos[1] // box_height
-        if not start_box_set or (col, row) != (start_box_col, start_box_row):
-            if not target_box_set or (col, row) != (target_box_col, target_box_row):
-                grid[col][row].remove_wall()
+        # remove start box
+        if (col, row) == start_box.get_pos():
+            start_box.set_start(False)
+            start_box.set_visited(False)
+            start_box_set = False
+            list.remove(start_box)
+            start_box = None
+            
+        # remove target box
+        elif (col, row) == target_box.get_pos():
+            target_box.set_target(False)
+            target_box_set = False
+            target_box = None
+            
+        # remove walls
+        elif grid[col][row].is_wall():
+            grid[col][row].set_wall(False)
         
-    return start_box_set, target_box_set, start_box, target_box, start_box_col, start_box_row, target_box_col, target_box_row
+        else:
+            return start_box_set, target_box_set, start_box, target_box
+        
+    return start_box_set, target_box_set, start_box, target_box
 
 # djikstra algorithm
 def djikstra(list, start_box, target_box, path):
     if len(list):           # run till queue is not empty
         current_box = list.pop(0)           # remove the front box of the queue
-        current_box.set_visited()           # mark the current box as visited
+        current_box.set_visited(True)           # mark the current box as visited
         
         if current_box == target_box:               # if target is found then retrace the path back to source
             while current_box.get_prior() != start_box:
@@ -134,7 +168,7 @@ def djikstra(list, start_box, target_box, path):
         
         for neighbour in current_box.neighbours:        # iterate on all the valid neighbours of box
             if not neighbour.is_queued() and not neighbour.is_wall():
-                neighbour.set_queued()              # queue the neighbour if its neither queued nor a wall
+                neighbour.set_queued(True)              # queue the neighbour if its neither queued nor a wall
                 neighbour.set_prior(current_box)        # assign the current box as prior of the neighbour
                 list.append(neighbour)          # insert it in the queue
                                 
@@ -144,7 +178,7 @@ def djikstra(list, start_box, target_box, path):
 def dfs(list, start_box, target_box, path):
     if len(list):           # run till stack is not empty
         current_box = list.pop()            # remove the top box of the stack
-        current_box.set_visited()           # mark the current box as visited
+        current_box.set_visited(True)           # mark the current box as visited
         
         if current_box == target_box:               # if target is found then retrace the path back to source
             while current_box.get_prior() != start_box:
@@ -154,7 +188,7 @@ def dfs(list, start_box, target_box, path):
         
         for neighbour in current_box.neighbours:                # iterate on all the valid neighbours of box
             if not neighbour.is_queued() and not neighbour.is_wall():
-                neighbour.set_queued()              # queue the neighbour if its neither queued nor a wall
+                neighbour.set_queued(True)              # queue the neighbour if its neither queued nor a wall
                 neighbour.set_prior(current_box)        # assign the current box as prior of the neighbour
                 list.append(neighbour)          # insert it in the stack
 
@@ -164,7 +198,7 @@ def dfs(list, start_box, target_box, path):
 def bfs(list, start_box, target_box, path):
     if len(list):
         current_box = list.pop(0)            # remove the front box of the queue
-        current_box.set_visited()            # mark the current box as visited
+        current_box.set_visited(True)            # mark the current box as visited
         
         if current_box == target_box:               # if target is found then retrace the path back to source
             while current_box.get_prior() != start_box:
@@ -174,7 +208,7 @@ def bfs(list, start_box, target_box, path):
         
         for neighbour in current_box.neighbours:
             if not neighbour.is_queued() and not neighbour.is_wall():
-                neighbour.set_queued()              # queue the neighbour if its neither queued nor a wall
+                neighbour.set_queued(True)              # queue the neighbour if its neither queued nor a wall
                 neighbour.set_prior(current_box)        # assign the current box as prior of the neighbour
                 list.append(neighbour)          # insert it in the queue
                 
@@ -227,10 +261,6 @@ def main():
     target_box_set = False          # to check if target box is assigned or not
     begin_search = 0                # search start key(1, 2, 3, 4)
     start_box = None                # start box object
-    start_box_col = None            # x-coordinate of the start box
-    start_box_row = None            # y-coordinate of the start box
-    target_box_col = None           # x-coordinate of the target box
-    target_box_row = None           # y-coordinate of the target box
     target_box = None               # target box object
     
     # main iterator start(keeps the pygame window open until closed)
@@ -241,7 +271,7 @@ def main():
                 sys.exit()
                     
             elif event.type == pygame.MOUSEBUTTONDOWN:      # all mouse button press operations
-                start_box_set, target_box_set, start_box, target_box, start_box_col, start_box_row, target_box_col, target_box_row = handle_mouse_click(event, grid, start_box_set, target_box_set, start_box, target_box, list, box_width, box_height, start_box_col, start_box_row, target_box_col, target_box_row)
+                start_box_set, target_box_set, start_box, target_box = handle_mouse_click(event, grid, start_box_set, target_box_set, start_box, target_box, list, box_width, box_height)
             
             elif event.type == pygame.MOUSEMOTION:          # mouse motion for drawing the wall
                 x = pygame.mouse.get_pos()[0]
@@ -252,9 +282,9 @@ def main():
                     col = x // box_width
                     row = y // box_height
                     # avoiding start and target points
-                    if(col, row) != (start_box_col, start_box_row):
-                        if(col, row) != (target_box_col, target_box_row): 
-                            grid[col][row].set_wall()
+                    if(col, row) != start_box.get_pos():
+                        if(col, row) != target_box.get_pos(): 
+                            grid[col][row].set_wall(True)
                         
             # keyboard input
             elif event.type == pygame.KEYDOWN:
@@ -263,7 +293,7 @@ def main():
                     main()
                 
                 # algorithms initialization keys
-                elif event.key == pygame.K_0 and target_box_set:    # start / stop the current executing algorithm
+                elif event.key == pygame.K_0 and target_box_set:    # stop the current executing algorithm
                     begin_search = 0
                 elif event.key == pygame.K_1 and target_box_set:    # djikstra algorithm
                     begin_search = 1
@@ -281,6 +311,7 @@ def main():
         if len(list) == 0 and begin_search:
             Tk().wm_withdraw()
             messagebox.showinfo("Alert", "No path Found!")
+            begin_search = 0
         
         # creating blank window
         window.fill((120, 120, 120))
